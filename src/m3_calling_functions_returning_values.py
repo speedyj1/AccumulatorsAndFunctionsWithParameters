a337586413241b451b1ebae60c6d4d2ae9de1473
speedyj1 <speedyj1@rose-hulman.edu>
2017-11-30 11:00:17 -0500

    progress

--- src/m3_calling_functions_returning_values.py
+++ src/m3_calling_functions_returning_values.py
@@ -7,8 +7,8 @@ This module demonstrates and practices:
   -- UNIT TESTING.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jack Speedy.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m3t_tester
 
@@ -37,7 +37,7 @@ def main():
 def run_test_sum_of_digits():
     """ Tests the  sum_of_digits   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function, as follows:
+    # DONE: 2. Implement this TEST function, as follows:
     #
     #  Step 1:  This TEST function tests the  sum_of_digits  function.
     #    So read the doc-string of the  sum_of_digits  function
@@ -71,7 +71,23 @@ def run_test_sum_of_digits():
     # TO DO: 2 (continued).
     # Below this comment, add 3 more test cases of your own choosing.
     # ------------------------------------------------------------------
+    #Test 2:
+    expected = 12
+    answer = sum_of_digits(345)
+    print('Test 1 expected:', expected)
+    print('        actual:  ', answer)
 
+    #Test 3:
+    expected = 24
+    answer = sum_of_digits(888)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
+
+    #Test 4
+    expected = 6
+    answer = sum_of_digits(123)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
 
 def sum_of_digits(number):
     """
@@ -109,7 +125,7 @@ def sum_of_digits(number):
 def run_test_digits_in_cube():
     """ Tests the   digits_in_cube   function. """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function.
+    # DONE: 3. Implement this function.
     #   It TESTS the  digits_in_cube  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -134,6 +150,23 @@ def run_test_digits_in_cube():
     print('Testing the   digits_in_cube   function:')
     print('-----------------------------------------------------')
 
+    #Test 2:
+    expected = 8
+    answer = digits_in_cube(5)
+    print('Test 1 expected:', expected)
+    print('        actual:  ', answer)
+
+    #Test 3:
+    expected = 9
+    answer = digits_in_cube(3)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
+
+    #Test 4
+    expected = 8
+    answer = digits_in_cube(2)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
 
 def digits_in_cube(n):
     """
@@ -144,8 +177,11 @@ def digits_in_cube(n):
       If the integer (n) is 5    (so n cubed is 125),
       this function returns (1 + 2 + 5), which is 8.
     """
+    cubed = n**3
+    digit = sum_of_digits(cubed)
+    return digit
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
@@ -159,7 +195,7 @@ def digits_in_cube(n):
 def run_test_digits_in_power():
     """ Tests the   digits_in_power   function. """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement this function.
+    # DONE: 5. Implement this function.
     #   It TESTS the  digits_in_power  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -170,6 +206,23 @@ def run_test_digits_in_power():
     print('Testing the   digits_in_power   function:')
     print('--------------------------------------------------')
 
+    # Test 2:
+    expected = 7
+    answer = digits_in_power(5, 2)
+    print('Test 1 expected:', expected)
+    print('        actual:  ', answer)
+
+    # Test 3:
+    expected = 5
+    answer = digits_in_power(2, 5)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
+
+    # Test 4
+    expected = 9
+    answer = digits_in_power(3, 3)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
 
 def digits_in_power(n, k):
     """
@@ -182,8 +235,11 @@ def digits_in_power(n, k):
       this function returns 18
       since 12 to the 3rd power is 1728 (whose digits sum to 18).
     """
+    x = n**k
+    return sum_of_digits(x)
+
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #
     ####################################################################
     # IMPORTANT: CALL, as many times as needed,
@@ -206,6 +262,24 @@ def run_test_fancy_sums_of_digits():
     print('--------------------------------------------------')
     print('Testing the   fancy_sums_of_digits   function:')
     print('--------------------------------------------------')
+
+    # Test 2:
+    expected = 7
+    answer = fancy_sums_of_digits(10)
+    print('Test 1 expected:', expected)
+    print('        actual:  ', answer)
+
+    # Test 3:
+    expected = 19084
+    answer = fancy_sums_of_digits(2)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
+
+    # Test 4
+    expected = 124309
+    answer = fancy_sums_of_digits(35)
+    print('Test 1 expected:', expected)
+    print('         actual: ', answer)
 
     # ------------------------------------------------------------------
     # HINT:  For your 1st test, consider  n=10.  Figure out BY HAND
@@ -240,6 +314,11 @@ def fancy_sums_of_digits(n):
             -- the sum of the digits in (X ** Y) is 124309 (trust me!)
             -- so this function returns 124309.
     """
+    X = n**1000
+    Y = n**99
+    x = digits_in_power(X, Y)
+    sum = sum_of_digits(x)
+    return sum
     # ------------------------------------------------------------------
     # TODO: 8. Implement and test this function.
     #
